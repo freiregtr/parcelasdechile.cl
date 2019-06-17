7547926f1b29025bcf9dcd6ff386073219caf6d3
Ramón Freire <ramon.freire.o@gmail.com>
2019-06-17 10:46:25 -0400

    adicion de margarita al chat de parcelasdechile

--- view/modulos/chat.php
+++ view/modulos/chat.php
@@ -1,104 +1,134 @@
 <div id="sochSection">
-            <div class="sochIcon myIcon">
-                <span class="fa fa-whatsapp"></span>
+    <div class="sochIcon myIcon">
+        <span class="fa fa-whatsapp"></span>
+    </div>
+    <!-- íconos -->
+    <div class="chatIcons">
+        <div id="messengerIcon" class="myIcon" data-show="messengerPopup">
+            <span class="fab fa-facebook"></span>
+        </div>
+        <div id="whatsappIcon" class="myIcon" data-show="whatsappPopup">
+            <span class="fab fa-whatsapp"></span>
+        </div>
+    </div>
+    <div class="chatsPopupsBox">
+        <!-- Whatsapp popup -->
+        <div id="whatsappPopup" class="chatsPopup">
+            <div class="chatsPopup-header">
+                <div class="closePopup">
+                    <a href="javascript:;">
+                        <span class="fas fa-times"></span>
+                    </a>
+                </div>
+                <h2 class="chatsPopup-title">¿Necesitas ayuda? <br>habla con nosotros</h2>
+                <h2 class="chatsPopup-tagline">Haga clic en uno de nuestros representantes a continuación</h2>
             </div>
-            <!-- íconos -->
-            <div class="chatIcons">
-                <div id="messengerIcon" class="myIcon" data-show="messengerPopup">
-                    <span class="fab fa-facebook"></span>
-                </div>
-                <div id="whatsappIcon" class="myIcon" data-show="whatsappPopup">
-                    <span class="fab fa-whatsapp"></span>
-                </div>
-            </div>
-            <div class="chatsPopupsBox">
-                <!-- Whatsapp popup -->
-                <div id="whatsappPopup" class="chatsPopup">
-                    <div class="chatsPopup-header">
-                        <div class="closePopup">
-                            <a href="javascript:;">
-                                <span class="fas fa-times"></span>
-                            </a>
-                        </div>
-                        <h2 class="chatsPopup-title">¿Necesitas ayuda? <br>habla con nosotros</h2>
-                        <h2 class="chatsPopup-tagline">Haga clic en uno de nuestros representantes a continuación</h2>
-                    </div>
-                    <div class="chatsPopup-body">
-                        <div class="chatsPopupContent">
-                            <div class="chatsPopupList">
-                                <ul class="list-unstyled">
-                                    <li>
-                                        <div class="chats-button" data-contact="+56975194031" data-available='{"monday":"09:00-21:00", "tuesday":"09:00-21:00", "wednesday":"09:00-21:00", "thursday":"09:00-21:00", "friday":"09:00-21:00","saturday": "09:00-15:00" }'>
-                                            <div class="chatsUserImg">
-                                                <img src="view/componentes/img/chat/barbara.jpg" alt="" class="img-fluid">
-                                            </div>
-                                            <div class="chatsUsercontent">
-                                                <h2 class="chatsUserName">Barbara</h2>
-                                                <h3 class="chatsUserTagline">Vendedora</h3>
-                                                <span class="userStatus">Voleré luego.</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="chats-button" data-contact="+56993590127" data-available='{"monday":"09:00-19:00", "tuesday":"09:00-19:00", "wednesday":"09:00-19:00", "thursday":"09:00-19:00", "friday":"09:00-19:00","saturday": "10:00-15:00" }'>
-                                            <div class="chatsUserImg">
-                                                <img src="view/componentes/img/chat/mauricio.jpg" alt="" class="img-fluid">
-                                            </div>
-                                            <div class="chatsUsercontent">
-                                                <h2 class="chatsUserName">Mauricio</h2>
-                                                <h3 class="chatsUserTagline">Vendedor</h3>
-                                                <span class="userStatus">Voleré luego.</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Messenger popup -->
-                <div id="messengerPopup" class="chatsPopup">
-                    <div class="chatsPopup-header">
-                        <div class="closePopup">
-                            <a href="javascript:;">
-                                <span class="fas fa-times"></span>
-                            </a>
-                        </div>
-                        <h2 class="chatsPopup-title">¿Necesitas ayuda? <br>habla con nosotros</h2>
-                        <h2 class="chatsPopup-tagline">Haga clic en uno de nuestros representantes a continuación</h2>
-                    </div>
-                    <div class="chatsPopup-body">
-                        <div class="chatsPopupContent">
-                            <div class="chatsPopupList">
-                                <ul class="list-unstyled">
-                                    <li>
-                                        <div class="chats-button" data-username="BarbaraZuloagaWinter" data-available='{"monday":"09:00-21:00", "tuesday":"09:00-21:00", "wednesday":"09:00-21:00", "thursday":"09:00-21:00", "friday":"09:00-21:00","saturday": "09:00-15:00" }'>
-                                            <div class="chatsUserImg">
-                                                <img src="view/componentes/img/chat/barbara.jpg" alt="" class="img-fluid">
-                                            </div>
-                                            <div class="chatsUsercontent">
-                                                <h2 class="chatsUserName">Barbara</h2>
-                                                <h3 class="chatsUserTagline">Vendedor</h3>
-                                                <span class="userStatus">Volveré luego.</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="chats-button" data-username="mauricio.beasainalegria" data-available='{"monday":"09:00-19:00", "tuesday":"09:00-19:00", "wednesday":"09:00-19:00", "thursday":"09:00-19:00", "friday":"09:00-19:00","saturday": "10:00-15:00" }'>
-                                            <div class="chatsUserImg">
-                                                <img src="view/componentes/img/chat/mauricio.jpg" alt="" class="img-fluid">
-                                            </div>
-                                            <div class="chatsUsercontent">
-                                                <h2 class="chatsUserName">Mauricio</h2>
-                                                <h3 class="chatsUserTagline">Vendedor</h3>
-                                                <span class="userStatus">Volveré luego.</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+            <div class="chatsPopup-body">
+                <div class="chatsPopupContent">
+                    <div class="chatsPopupList">
+                        <ul class="list-unstyled">
+                            <li>
+                                <div class="chats-button" data-contact="+56975194031" data-available='{"monday":"09:00-21:00", "tuesday":"09:00-21:00", "wednesday":"09:00-21:00", "thursday":"09:00-21:00", "friday":"09:00-21:00","saturday": "09:00-15:00" }'>
+                                    <div class="chatsUserImg">
+                                        <img src="view/componentes/img/chat/barbara.jpg" alt="" class="img-fluid">
+                                    </div>
+                                    <div class="chatsUsercontent">
+                                        <h2 class="chatsUserName">Barbara</h2>
+                                        <h3 class="chatsUserTagline">Vendedora</h3>
+                                        <span class="userStatus">Volveré luego.</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="chats-button" data-contact="+56993590127" data-available='{"monday":"09:00-19:00", "tuesday":"09:00-19:00", "wednesday":"09:00-19:00", "thursday":"09:00-19:00", "friday":"09:00-19:00","saturday": "10:00-15:00" }'>
+                                    <div class="chatsUserImg">
+                                        <img src="view/componentes/img/chat/mauricio.jpg" alt="" class="img-fluid">
+                                    </div>
+                                    <div class="chatsUsercontent">
+                                        <h2 class="chatsUserName">Mauricio</h2>
+                                        <h3 class="chatsUserTagline">Vendedor</h3>
+                                        <span class="userStatus">Volveré luego.</span>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div class="chats-button" data-contact="+56988214958" data-available='{"monday":"09:00-19:00", "tuesday":"09:00-19:00", "wednesday":"09:00-19:00", "thursday":"09:00-19:00", "friday":"09:00-19:00","saturday": "10:00-15:00" }'>
+                                    <div class="chatsUserImg">
+                                        <img src="view/componentes/img/chat/margarita.jpg" alt="" class="img-fluid">
+                                    </div>
+                                    <div class="chatsUsercontent">
+                                        <h2 class="chatsUserName">Margarita</h2>
+                                        <h3 class="chatsUserTagline">Vendedor</h3>
+                                        <span class="userStatus">Volveré luego.</span>
+                                    </div>
+                                </div>
+                            </li>
+
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- Messenger popup -->
+        <div id="messengerPopup" class="chatsPopup">
+            <div class="chatsPopup-header">
+                <div class="closePopup">
+                    <a href="javascript:;">
+                        <span class="fas fa-times"></span>
+                    </a>
+                </div>
+                <h2 class="chatsPopup-title">¿Necesitas ayuda? <br>habla con nosotros</h2>
+                <h2 class="chatsPopup-tagline">Haga clic en uno de nuestros representantes a continuación</h2>
+            </div>
+            <div class="chatsPopup-body">
+                <div class="chatsPopupContent">
+                    <div class="chatsPopupList">
+                        <ul class="list-unstyled">
+
+                            <li>
+                                <div class="chats-button" data-username="BarbaraZuloagaWinter" data-available='{"monday":"09:00-21:00", "tuesday":"09:00-21:00", "wednesday":"09:00-21:00", "thursday":"09:00-21:00", "friday":"09:00-21:00","saturday": "09:00-15:00" }'>
+                                    <div class="chatsUserImg">
+                                        <img src="view/componentes/img/chat/barbara.jpg" alt="" class="img-fluid">
+                                    </div>
+                                    <div class="chatsUsercontent">
+                                        <h2 class="chatsUserName">Barbara</h2>
+                                        <h3 class="chatsUserTagline">Vendedor</h3>
+                                        <span class="userStatus">Volveré luego.</span>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div class="chats-button" data-username="mauricio.beasainalegria" data-available='{"monday":"09:00-19:00", "tuesday":"09:00-19:00", "wednesday":"09:00-19:00", "thursday":"09:00-19:00", "friday":"09:00-19:00","saturday": "10:00-15:00" }'>
+                                    <div class="chatsUserImg">
+                                        <img src="view/componentes/img/chat/mauricio.jpg" alt="" class="img-fluid">
+                                    </div>
+                                    <div class="chatsUsercontent">
+                                        <h2 class="chatsUserName">Mauricio</h2>
+                                        <h3 class="chatsUserTagline">Vendedor</h3>
+                                        <span class="userStatus">Volveré luego.</span>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div class="chats-button" data-username="margarita.l.lagos" data-available='{"monday":"09:00-19:00", "tuesday":"09:00-19:00", "wednesday":"09:00-19:00", "thursday":"09:00-19:00", "friday":"09:00-19:00","saturday": "10:00-15:00" }'>
+                                    <div class="chatsUserImg">
+                                        <img src="view/componentes/img/chat/margarita.jpg" alt="" class="img-fluid">
+                                    </div>
+                                    <div class="chatsUsercontent">
+                                        <h2 class="chatsUserName">Margarita</h2>
+                                        <h3 class="chatsUserTagline">Vendedor</h3>
+                                        <span class="userStatus">Volveré luego.</span>
+                                    </div>
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
