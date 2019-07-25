9ab33aad27fa055bbe76f624427b7efad95b0f88
Jose Reyes <jreyesp96@gmail.com>
2019-07-25 12:10:47 -0400

    parcela 53 y 61 lago chapo vendidas

--- view/componentes/mapsvg/js/lagochapo.js
+++ view/componentes/mapsvg/js/lagochapo.js
@@ -159,9 +159,9 @@ jQuery(document).ready(function () {
             'P-53': {
                 id: "P-53",
                 'id_no_spaces': "P-53",
-                 fill: "rgba(83,147,67,1)",
-                tooltip: "P-53 $ 16.990.000",
-                popover: "P-53 $ 16.990.000",
+                fill: "rgba(88,89,89,1)",
+                tooltip: "P-53 VENDIDA",
+                popover: "P-53 VENDIDA",
                 data: {}
             },
             'P-54': {
@@ -199,9 +199,9 @@ jQuery(document).ready(function () {
             'P-61': {
                 id: "P-61",
                 'id_no_spaces': "P-61",
-                 fill: "rgba(83,147,67,1)",
-                tooltip: "P-61 $ 16.990.000",
-                popover: "P-61 $ 16.990.000",
+                 fill: "rgba(88,89,89,1)",
+                tooltip: "P-61 VENDIDA",
+                popover: "P-61 VENDIDA",
                 data: {}
             },
             'P-62': {
