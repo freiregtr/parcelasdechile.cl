ff79b159be2272130a0d0a8e6883e2cf95b43ae8
Daria Pankova <dariapankova@MacBook-Pro-Daria.local>
2019-12-27 09:48:31 -0300

    subiendo precio del volantin en inicio

--- view/modulos/inicio.php
+++ view/modulos/inicio.php
@@ -67,7 +67,7 @@
                             <hr class="divider veil reveal-md-inline-block">
                             <h1>Parcelación <br> El Volantín</h1>
                             <br>
-                            <p class="offset-top-35">Parcelas desde los $7.990.000, Un terreno es la mejor forma de asegurar el futuro de tu familia. A solo 138 km de Santiago y con buena conexión con otros destinos.</p>
+                            <p class="offset-top-35">Parcelas desde los $8.490.000, Un terreno es la mejor forma de asegurar el futuro de tu familia. A solo 138 km de Santiago y con buena conexión con otros destinos.</p>
                             <br>
                             <button type="button" class="btn btn-primary video-btn" data-toggle="modal" data-src="https://www.youtube.com/embed/n0ORmVVdVqY" data-target="#myModal">
                                 ver video
