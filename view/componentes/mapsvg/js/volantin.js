793926e3141259267e40ba31713cec193390610f
Daria Pankova <daria11pankova@gmail.com>
2021-01-06 09:11:38 -0300

    actualizacion planos

--- view/componentes/mapsvg/js/volantin.js
+++ view/componentes/mapsvg/js/volantin.js
@@ -302,9 +302,9 @@ jQuery(document).ready(function () {
             'P-10': {
                 id: "P-10",
                 'id_no_spaces': "P-10",
-                  fill: "rgba(25,143,61,1)",
-                tooltip: "$11.490.000",
-                popover: "$11.490.000",
+                  fill: "rgba(88,89,89,1)",
+                tooltip: "P-10 VENDIDA",
+                popover: "P-10 VENDIDA",
                 data: {}
             },
             'P-11': {
